10010ba8e1aea4b26a133dda89373139f49e10f4
fedir72 <fedir72@ua.fm>
2022-10-25 19:23:39 +0300

    empty project

---
+++ FoursquareAppSwiftUI/FoursquareAppSwiftUI/FoursquareAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoursquareAppSwiftUIApp.swift
+//  FoursquareAppSwiftUI
+//
+//  Created by Fedii Ihor on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoursquareAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
